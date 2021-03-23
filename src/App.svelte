9fe7dc4126e680eb5115efc66d2f335382271a10
elric1992 <elric2009@gmail.com>
2021-03-23 17:50:22 +1000

    add tailwind css

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,11 @@
 <script lang="ts">
-    import {format} from "date-fns";
-    import {send, state} from "./HabitMachine";
+    import { format, isSameMonth } from "date-fns";
+    import AddHabit from "./AddHabit.svelte";
     import Day from "./Day.svelte";
-
+    import { send, state } from "./HabitMachine";
     $: currentDate = $state.context.currentDate;
     $: currentMonth = $state.context.currentMonth;
-
+    $: habits = $state.context.habits;
     document.addEventListener("keydown", (e) => {
         switch (e.code) {
             case "ArrowLeft":
@@ -27,58 +27,43 @@
     });
 </script>
 
-<main>
-    <h1><b>Habit control</b></h1>
-    <h2>{format(currentDate, 'MMMM - yyyy')}</h2>
-    <div>
-        <button on:click={() => send({type: 'NAVIGATE', direction: 'BACK'})}>Back</button>
-        <button on:click={() => send({type: 'NAVIGATE', direction: 'HOME'})}>Home</button>
-        <button on:click={() => send({type: 'NAVIGATE', direction: 'FORWARD'})}>Forward</button>
-    </div>
-    <div>
-        <button on:click={() => send('EDIT')}>Add Habit</button>
+<main class="h-full text-white bg-gradient-to-b from-indigo-200 to-indigo-900">
+    <div
+            class="p-4 text-4xl leading-tight text-center bg-gradient-to-r from-orange-400 to-red-700">
+        <h1 class="text-6xl font-bold">HaBit Control</h1>
+        <h2>{format(currentDate, 'MMMM - yyyy')}</h2>
+
+        <div class="flex justify-center p-4 space-x-4">
+            <button on:click={() => send({ type: 'NAVIGATE', direction: 'BACK' })}>
+                Back
+            </button>
+            <button on:click={() => send({ type: 'NAVIGATE', direction: 'HOME' })}>
+                Home
+            </button>
+            <button on:click={() => send({ type: 'NAVIGATE', direction: 'FORWARD' })}>
+                Forward
+            </button>
+        </div>
+
+        <button on:click={() => send('EDIT')} class="p-4 bg-red-400 rounded">
+            Add Habit
+        </button>
         <button on:click={() => send('RESET')}>Reset</button>
     </div>
-    <div class="grid auto-fit">
-        {#each currentMonth as day}
-            <Day {day}/>
+
+    <div class="grid grid-cols-7 gap-2 p-4">
+        {#each currentMonth as day (day.toDateString())}
+            <Day {day} {habits} enabled={isSameMonth(day, currentDate)} />
         {/each}
     </div>
+
+    {#if $state.matches('edit')}
+        <AddHabit />
+    {/if}
 </main>
 
-<style>
-    main {
-        text-align: center;
-        padding: 1em;
-        /*max-width: 240px;*/
-        margin: 0 auto;
-    }
-
-    h1 {
-        color: #ff3e00;
-        text-transform: uppercase;
-        /*font-size: 4em;*/
-        font-weight: 100;
-    }
-
-    .grid {
-        display: grid;
-        grid-gap: 2vw;
-        margin: 5px;
-    }
-
-    .grid > div {
-        font-size: 5vw;
-        padding: .5em;
-        background: gold;
-        text-align: center;
-    }
-
-    .auto-fill {
-        grid-template-columns: repeat(auto-fill, minmax(150px, 1fr));
-    }
-
-    .auto-fit {
-        grid-template-columns: repeat(auto-fit, minmax(50px, 2fr));
-    }
+<style global>
+    @tailwind base;
+    @tailwind components;
+    @tailwind utilities;
 </style>
--- src/HabitMachine.ts
+++ src/HabitMachine.ts
@@ -1,63 +1,94 @@
-import {addDays, eachDayOfInterval, endOfMonth, endOfWeek, startOfMonth, startOfWeek} from "date-fns";
-import {assign, createMachine} from "xstate";
-import {useMachine} from "xstate-svelte";
+import {
+    addDays,
+    eachDayOfInterval,
+    endOfMonth,
+    endOfWeek,
+    isToday,
+    startOfMonth,
+    startOfWeek,
+} from "date-fns";
+import { assign, createMachine } from "xstate";
+import { useMachine } from "xstate-svelte";
+import type { Habit } from "./Habit";
 
-export interface Habit {
-    name: string;
-}
+const loadHabits = () =>
+    (JSON.parse(localStorage.getItem("habits")) ?? []) as Habit[];
+const saveHabits = (habits: Habit[]) =>
+    localStorage.setItem("habits", JSON.stringify(habits));
 
 export interface AppContext {
     currentDate: Date;
-    currentMonth: Date[],
+    currentMonth: Date[];
     habits: Habit[];
 }
 
 export type AppEvent =
-    | { type: 'NAVIGATE'; direction: 'BACK' | 'HOME' | 'FORWARD' }
-    | { type: 'EDIT' };
+    | { type: "NAVIGATE"; direction: "BACK" | "HOME" | "FORWARD" }
+    | { type: "EDIT" }
+    | { type: "CANCEL_EDIT" }
+    | { type: "ADD_HABIT"; habit: Habit }
+    | { type: "RESET" };
 
 const buildContext = (date: Date, habits: Habit[]): AppContext => ({
     currentDate: date,
     currentMonth: eachDayOfInterval({
-        start: startOfWeek(startOfMonth(date), {weekStartsOn: 1}),
-        end: endOfWeek(endOfMonth(date), {weekStartsOn: 1})
+        start: startOfWeek(startOfMonth(date), { weekStartsOn: 1 }),
+        end: endOfWeek(endOfMonth(date), { weekStartsOn: 1 }),
     }),
-    habits
-})
+    habits,
+});
 
 export const habitMachine = createMachine<AppContext, AppEvent>({
-    initial: 'idle',
-    context: buildContext(new Date(), []),
+    initial: "idle",
+    context: buildContext(new Date(), loadHabits()),
     states: {
         idle: {
             on: {
                 NAVIGATE: {
                     actions: assign((ctx, evt) => {
                         switch (evt.direction) {
-                            case 'BACK':
+                            case "BACK":
                                 return buildContext(
                                     addDays(ctx.currentMonth[0], -1),
                                     ctx.habits
                                 );
-                            case 'HOME':
-                                return buildContext(
-                                    new Date(),
-                                    ctx.habits
-                                );
-                            case 'FORWARD':
+                            case "HOME":
+                                return buildContext(new Date(), ctx.habits);
+                            case "FORWARD":
                                 return buildContext(
                                     addDays(ctx.currentMonth[ctx.currentMonth.length - 1], 1),
                                     ctx.habits
                                 );
                         }
-                    })
+                    }),
+                    cond: (ctx, evt) =>
+                        !(evt.direction === "HOME" && isToday(ctx.currentDate)),
                 },
-                EDIT: 'edit'
-            }
+                EDIT: "edit",
+                RESET: {
+                    actions: [
+                        assign(buildContext(new Date(), [])),
+                        () => localStorage.clear(),
+                    ],
+                },
+            },
         },
-        edit: {}
-    }
+        edit: {
+            on: {
+                CANCEL_EDIT: "idle",
+                ADD_HABIT: {
+                    target: "idle",
+                    actions: [
+                        assign({
+                            habits: (ctx, evt) => ctx.habits.concat([evt.habit]),
+                        }),
+                        (ctx) => saveHabits(ctx.habits),
+                    ],
+                },
+            },
+        },
+    },
 });
 
-export const {state, send} = useMachine(habitMachine);
+export const { state, send } = useMachine(habitMachine);
 
