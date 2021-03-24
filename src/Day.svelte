f81c59d9203d2ef0b82ee9e5774931861d2fb775
elric1992 <elric2009@gmail.com>
2021-03-24 13:09:16 +1000

    add habit

--- src/Day.svelte
+++ src/Day.svelte
@@ -1,11 +1,53 @@
 <script lang="ts">
-    import {format} from "date-fns";
+    import {format, isToday} from "date-fns";
+    import type {Habit} from "./Habit";
 
+    let num = 222222;
     export let day: Date;
+    export let habits: Habit[];
+    export let enabled: boolean;
+
+    const key = day.toDateString();
+
+    $: today = isToday(day);
+    $: localHabits = habits;
+    $: completedHabits = (JSON.parse(localStorage.getItem(key)) ?? []) as Habit[];
+
+    const sameHabits = (habit1: Habit, habit2: Habit) =>
+        JSON.stringify(habit1) === JSON.stringify(habit2);
+
+    const isHabitCompleted = (habit: Habit) =>
+        completedHabits.some((ch) => sameHabits(ch, habit));
+
+    const toggleHabit = (habit: Habit) => {
+        if (isHabitCompleted(habit)) {
+            completedHabits = completedHabits.filter((ch) => !sameHabits(ch, habit));
+            if (completedHabits.length > 0)
+                localStorage.setItem(key, JSON.stringify(completedHabits));
+            else localStorage.removeItem(key);
+        } else {
+            completedHabits = completedHabits.concat([habit]);
+            localStorage.setItem(key, JSON.stringify(completedHabits));
+        }
+        localHabits = habits;
+    };
+
 </script>
 
-
-<div class="day">{format(day, 'EEE. do')}</div>
+<!--<div>{num}</div>-->
+{#if enabled}
+    <main>
+        <div class="day">{format(day, 'EEE. do')}
+<!--        <div class="day">-->
+            {#each localHabits as habit}
+                <div on:click={() => toggleHabit(habit)}>{habit.name}</div>
+            {/each}
+<!--        </div>-->
+        </div>
+    </main>
+{:else}
+    <main/>
+{/if}
 
 <style>
     div.day {
@@ -16,6 +58,7 @@
         border-radius: 10px;
         background-color: #ffebcd;
     }
+
     div.day:hover {
         background-color: #999999;
     }
